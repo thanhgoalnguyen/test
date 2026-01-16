5bb575a571cb915b00ca912bbe611fbb97edc7c7
thanhgoalnguyen <nguyenngocthanh137@gmail.com>
2026-01-16 14:45:00 +0700

    3

--- script.js
+++ script.js
@@ -8,4 +8,42 @@ document.addEventListener("DOMContentLoaded", function () {
 			parent.classList?.toggle('active');
 		})
 	})
+
+	const observerOptions = {
+		threshold: 0.2
+	};
+
+	const observer = new IntersectionObserver((entries) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				entry.target.classList.add('show');
+				observer.unobserve(entry.target);
+			}
+		});
+	}, observerOptions);
+
+	document.querySelectorAll('.js-show').forEach((el) => {
+		observer.observe(el);
+	});
+
+	const swiper = new Swiper('.mySwiper', {
+		direction: 'horizontal',
+		slidesPerView: 1.4,
+    	spaceBetween: 13,
+		loop: true,
+		breakpoints: {
+			834: {
+				slidesPerView: 1.21,
+			},
+			1440: {
+				slidesPerView: 1.275,
+			},
+		},
+
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+		centeredSlides: true,
+  	});
 })
