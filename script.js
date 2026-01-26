b565716144e67d1977c9f00fdbad986418c363e8
thanhgoalnguyen <nguyenngocthanh137@gmail.com>
2026-01-27 00:44:04 +0700

    Feature: build landing page

--- script.js
+++ script.js
@@ -1088,7 +1088,7 @@ document.addEventListener("DOMContentLoaded", function () {
 	})
 
 	const observerOptions = {
-		threshold: 0.3
+		threshold: 0.4
 	};
 
 	const observer = new IntersectionObserver((entries) => {
