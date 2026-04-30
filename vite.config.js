77d3701d8fd9f2404b8be9c523568db4f2e80c7b
thanhgoalnguyen <nguyenngocthanh137@gmail.com>
2026-05-01 00:01:17 +0700

    3

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,15 @@ export default defineConfig({
     rollupOptions: {
       output: {
         // Gom các file lẻ thành tên cố định hoặc có hash
-		assetFileNames: 'assets/[path][name].[ext]',
+		assetFileNames: (assetInfo) => {
+          let name = assetInfo.name;
+          // Giữ nguyên cấu trúc thư mục cho ảnh
+          if (/\.(avif|png|jpe?g|svg|gif)$/.test(name)) {
+            return `assets/[name][extname]`; 
+          }
+          // Các file khác (css, font...) thì để mặc định
+          return `assets/[name]-[hash][extname]`;
+        },
         chunkFileNames: 'assets/[name].js',
         entryFileNames: 'assets/[name].js',
       },
